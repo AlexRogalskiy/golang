4bdca9daeb0475171f374c562bdccfdd2eac0045
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-10-24 23:22:44 +0300

    C

--- case_02.go
+++ case_02.go
@@ -66,8 +66,17 @@ func main() {
             fmt.Println("received", msg2)
         }
     }
-    
+
     //-----------------------------------
+
+    queue := make(chan string, 2)
+    queue <- "one"
+    queue <- "two"
+    close(queue)
+    for elem := range queue {
+        fmt.Println(elem)
+    }
+    
     //-----------------------------------
     //-----------------------------------
     //-----------------------------------
---
+++ case_06.go
@@ -0,0 +1,34 @@
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+
+    timer1 := time.NewTimer(time.Second * 2)
+
+    <-timer1.C
+    fmt.Println("Timer 1 expired")
+
+    timer2 := time.NewTimer(time.Second)
+    go func() {
+        <-timer2.C
+        fmt.Println("Timer 2 expired")
+    }()
+    stop2 := timer2.Stop()
+    if stop2 {
+        fmt.Println("Timer 2 stopped")
+    }
+
+    //-----------------------------------
+
+    ticker := time.NewTicker(time.Millisecond * 500)
+    go func() {
+        for t := range ticker.C {
+            fmt.Println("Tick at", t)
+        }
+    }()
+    time.Sleep(time.Millisecond * 1600)
+    ticker.Stop()
+    fmt.Println("Ticker stopped")
+}
